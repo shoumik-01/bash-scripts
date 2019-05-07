09b40cd56f0fbd086b4debe7da2a33edce4c9f94
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-05-07 01:34:04 -0400

    Used better coding practice

--- pingmap.sh
+++ pingmap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-printf "PINGMAP V1.0"
+printf "PINGMAP V1.1"
 printf "\nIP address of the host to be monitored: "
 read IP
+printf "\nPing interval: "
+read delay
 printf "\nSave log file at: "
 read dir
 printf "\nName of log file: "
@@ -10,14 +12,13 @@ read file
 touch $dir/$file
 while true
 do
+        doot=$(date +"[%d/%b/%Y:%k:%M:%S %Z]")
         ping -c 4 "$IP" > /dev/null
         if [ $? -eq 0 ]; then
-                doot=$(date +"[%d/%b/%Y:%k:%M:%S %Z]")
                 echo "$doot: $IP is up" >> $dir/$file
         else
-                doot=$(date +"[%d/%b/%Y:%k:%M:%S %Z]")
                 echo "$doot: $IP is down" >> $dir/$file
         fi
         echo "------" >> $dir/$file
-        sleep 30
+        sleep $delay
 done
