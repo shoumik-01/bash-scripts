633bfdb5e2efe7bf5dabbef88c9a7f0c43ea722c
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-05-07 02:13:08 -0400

    Netcat based script to verify open port

---
+++ verify_open_port.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+printf "\nPort type (t/u): "
+read type
+printf "\nPort to be scanned: "
+read port
+printf "\nPath to list (absolute path /dir1/dir2/filename): "
+read path
+
+case $type in
+        "t") for i in `cat $path`; do echo -e results for $i '\n'; nc -z -v -w5 $i $port; echo -e '\n' ------; done;;
+        "u") for i in `cat $path`; do echo -e results for $i '\n'; nc -z -v -w5 -u $i $port; echo -e '\n' ------; done;;
+        *) echo 'Unknown port type, try "t" for TCP and "u" for UDP';;
+esac
