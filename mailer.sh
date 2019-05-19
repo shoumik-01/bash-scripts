9f6d0d60f845d62df219eb2892521190c736b5fb
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-05-19 07:51:06 -0400

    A spamming script good for nothing

---
+++ mailer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+printf "\nEnter email address: "
+read email
+printf "\nHow many emails should be sent: "
+read n
+printf "\nDelay between emails (0 for no delay): "
+read delay
+
+
+i=1
+while [ $i -le $n ]
+do
+        echo "This is a test email. No action is required." | mail -s "Test email pls ignore" $email
+        sleep $delay
+        (( i++ ))
+done
