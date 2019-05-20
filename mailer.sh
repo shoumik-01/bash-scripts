d62e4565f2bed6e16aaef97118509b03f4f19df1
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-05-19 20:49:08 -0400

    Made the script a bit more annoying

--- mailer.sh
+++ mailer.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-printf "\nEnter email address: "
+printf "\nEnter recipient email address: "
 read email
+printf "\nFrom address: "
+read from
 printf "\nHow many emails should be sent: "
 read n
 printf "\nDelay between emails (0 for no delay): "
@@ -10,7 +12,7 @@ read delay
 i=1
 while [ $i -le $n ]
 do
-        echo "This is a test email. No action is required." | mail -s "Test email pls ignore" $email
+        echo "This is a test email. No action is required." | mail -s "Test email pls ignore" -r $from $email
         sleep $delay
         (( i++ ))
 done
