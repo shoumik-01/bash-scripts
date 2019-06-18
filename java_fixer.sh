bf77a2a0243d1a333ee8426c6d3080c98a5dc276
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-06-18 18:32:01 -0400

    Fixed some bugs incase more openjdk in installed

--- java_fixer.sh
+++ java_fixer.sh
@@ -5,6 +5,6 @@ CURRENT_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep '
 yum -y install java-1.8.0-openjdk
 unlink /etc/alternatives/java
 ln -s $CURRENT_HOME/bin/java /etc/alternatives/java
-OPEN_HOME=$(find /usr/lib/jvm -type d -name "*openjdk*")
-LINK_NAME=$(rpm -qa | grep openjdk | head -1 | cut -c 12-28)
+OPEN_HOME=$(find /usr/lib/jvm -type d -name "*openjdk-1.8*")
+LINK_NAME=$(rpm -qa | grep openjdk-1.8 | cut -c 12-28)
 ln -s $OPEN_HOME/jre/bin /opt/$LINK_NAME
