b714148c0394ec56665684205014dd7046794f44
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-07-28 04:24:12 -0400

    Update vitalwatchv2.sh

--- vitalwatchv2.sh
+++ vitalwatchv2.sh
@@ -6,7 +6,7 @@ k=1024
 while true
 do
         DATE=$(date +"[%d/%b/%Y:%k:%M:%S %Z]")
-        CPU=$(top -n1 | grep -i cpu | head -1 | awk '{print $2}')
+        CPU=$(top -b -n1 | grep -i cpu | head -1 | awk '{print $2}')
         MEMK=$(vmstat -s | head -2 | grep used | awk '{print $1}')
         MEMB=$(awk -v dividend="${MEMK}" -v divisor="${k}" 'BEGIN {printf "%.2f", dividend/divisor; exit(0)}')
         echo "$DATE: $CPU, $MEMB" >> $OUTPUT
