3c10ba48b138d575ac2b7b14f398002c65f8a8c2
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-08-05 17:39:33 -0400

    Basic info and dns check

--- sysinfo.sh
+++ sysinfo.sh
@@ -29,13 +29,46 @@ LOC_FS=$(df -lT -xtmpfs -xdevtmpfs -xcifs | awk -v OFS='\t' '{print $7,$2,$6}')
 DEF_GW=$(ip r l | grep via | awk '{for (i=1;i<=NF;i++) if ($i == "via") {print $(i+1)};}')
 DNS_SRV1=$(grep "^[^#;]" /etc/resolv.conf | grep name | head -1 | awk '{print $2}')
 DNS_SRV2=$(grep "^[^#;]" /etc/resolv.conf | grep name | tail -1 | awk '{print $2}')
+function link_detect () {
+        ping -c 2 "$DEF_GW" > /dev/null
+        if [ $? -eq 0 ]; then
+                printf "\nGateway is reachable."
+        else
+                printf "\nGateway is unreachable."
+        fi
+}
+function dns_check () {
+        rev_lookup=$(nslookup $ADDR $DNS_SRV1 | grep "name" | awk '{print $4}' | sed 's/.$//')
+        for_lookup=$(nslookup $HOST $DNS_SRV2 | tail -2 | grep -i addr | awk '{print $2}')
+        if [ "$rev_lookup" == "$HOST" ] && [ "$for_lookup" == "$ADDR" ]
+        then
+                printf "\nForward and Reverse lookups are good!"
+        else
+                printf "\nDNS mismatch. Please investigate."
+        fi
+}
 
 ##Process##
 
-HIGH_CPU=$(ps -eo pcpu,pid,user,args | sort -k 1 -r | head -5)
-HIGH_MEM=$(ps -eo %mem,pid,user,args | sort -k 1 -r | head -5)
+HIGH_CPU=$(ps -eo pcpu,pid,user,args | sort -k 1 -r | head -6)
+HIGH_MEM=$(ps -eo %mem,pid,user,args | sort -k 1 -r | head -6)
 
 ##Display info##
 
-printf "\nHostname: $HOST \nUptime: $UPTIME $UPTIME_DAY \nIP: $ADDR \nCPU info: $CPU_CORES \nTotal memory: $TOTAL_MEM \nUsed memory: $USED_MEM \nCurrent CPU usage: $USED_CPU \nOS: $OS \nKernel version: $KERNEL"
-printf "\nHardware info: $DMI"
+printf "\nHostname: $HOST \nUptime: $UPTIME $UPTIME_DAY \nIP: $ADDR \nCPU info: --↓ \n$CPU_CORES \nTotal memory: $TOTAL_MEM \nUsed memory: $USED_MEM K \nCurrent CPU usage: $USED_CPU%% \nOS: --↓ \n$OS \nKernel version: $KERNEL"
+printf "\nHardware info: --↓ \n$DMI"
+
+printf "\nPhysical disks: --↓ \n$DISKS \n"
+printf '%s\n' "${LOC_FS}"
+disk_health
+
+printf "\nTop 5 CPU intensive processes: --↓ \n"
+printf '%s\n' "$HIGH_CPU \n"
+
+printf "\nTop 5 memory intensive processes: --↓ \n"
+printf '%s\n' "$HIGH_MEM \n"
+
+link_detect
+dns_check
+
+echo -e "\nEnd Of Sysinfo"
