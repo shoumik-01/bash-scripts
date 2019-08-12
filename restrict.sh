1ff967f655c049d69164b47e357939164d69549e
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2019-08-12 17:43:18 -0400

    Restricted bash

--- restrict.sh
+++ restrict.sh
@@ -6,13 +6,18 @@ ls -1 /home | grep -Ev "svc_unix|hasans" >> /tmp/users.txt
 
 for i in `cat /tmp/users.txt`
 do
-        mkdir -p /home/$i/programs
-        ln -s /usr/local/bin/ssh /home/$i/programs/
-        ln -s /bin/whoami /home/$i/programs/
-        ln -s /bin/sed /home/$i/programs/
-        ln -s /bin/logger /home/$i/programs/
-        mv /home/$i/.bash_profile /home/$i/.bash_profile_bkup
-        cp /root/.bash_profile_restricted /home/$i/.bash_profile
-        chattr +i /home/$i/.bash_profile
+        grep "^[^#;]" /home/$i/.bash_profile | grep "programs" > /dev/null
+        if [ $? -eq 0 ]; then
+                sleep 1
+        else
+                mkdir -p /home/$i/programs
+                ln -s /usr/local/bin/ssh /home/$i/programs/
+                ln -s /bin/whoami /home/$i/programs/
+                ln -s /bin/sed /home/$i/programs/
+                ln -s /bin/logger /home/$i/programs/
+                mv /home/$i/.bash_profile /home/$i/.bash_profile_bkup
+                cp /root/.bash_profile_restricted /home/$i/.bash_profile
+                chattr +i /home/$i/.bash_profile
+        fi
 done
 rm -f /tmp/users.txt
