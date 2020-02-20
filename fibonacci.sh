21c3d5ddc8aa30e088a79ffd222a970d28bf4045
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2020-02-20 17:49:25 -0600

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+f0=0
+f1=1
+i=1
+
+printf "\nHow many Fibonacci numbers do you want (max 93): "
+read n
+
+while [ $i -le $n ]
+do
+        printf "$f0 \n"
+        fnew=$(bc <<< "$f0+$f1")
+        f0=$f1
+        f1=$fnew
+        (( i++ ))
+done
