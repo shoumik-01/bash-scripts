0f07c1940a600d7bb2a2c81a72459d2d200853b5
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2021-10-24 14:16:35 -0500

    Update process_wrapper.sh
    Added filesize functionality

--- process_wrapper.sh
+++ process_wrapper.sh
@@ -2,9 +2,11 @@
 
 PRCS=$(ps | grep normie_watch | grep -v "grep")
 PID=$(ps | grep normie_watch | grep -v "grep" | awk '{print $1}')
+FILESIZE=$(ls -lh /tmp/trackshim.log | awk '{print $5}')
 
 function action_running() {
         printf "\nCurrently running normie related process is $PRCS\n"
+        printf "\nLogfile size is $FILESIZE\n"
         printf "\nKill the process? y/n \n"
         read cmd
         case $cmd in
@@ -15,7 +17,9 @@ function action_running() {
 }
 
 function action_down() {
-        printf "\nNo normie related processes are running. Start process? y/n \n"
+        printf "\nNo normie related processes are running.\n"
+        printf "\nOld logfile size is $FILESIZE\n"
+        printf "\nStart the process? y/n \n"
         read cmd
         case $cmd in
                 "y") /root/scripts/normie_watch.sh &;;
